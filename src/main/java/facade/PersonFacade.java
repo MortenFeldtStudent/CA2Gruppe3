2abf2b71f3e3811985e76d1bfe7e736743e7f9b0
Jörg <oerteljoerg@gmail.com>
2019-03-12 14:40:39 +0100

    Person Interface

--- src/main/java/facade/PersonFacade.java
+++ src/main/java/facade/PersonFacade.java
@@ -1,5 +1,33 @@
 package facade;
 
-public class PersonFacade {
+import entity.Person;
+import java.util.List;
+
+public class PersonFacade implements interfaces.IPersonFacade{
+
+    @Override
+    public Person getInfoFromPersonByPhoneNumber(int phoneNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> getAllPersonsByHobby(String hobby) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person getPersonByCity(String cityName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long getCountOfPeopleWithGivenHobby(String hobby) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> getAllZipFromCountry(int zipCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
