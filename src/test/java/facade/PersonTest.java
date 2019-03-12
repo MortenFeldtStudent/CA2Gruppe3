27ee8bef400b35e0c7df4fdb8d950e308e65920b
Jörg <oerteljoerg@gmail.com>
2019-03-12 14:38:38 +0100

    Person Interface

--- src/test/java/facade/PersonTest.java
+++ src/test/java/facade/PersonTest.java
@@ -4,9 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class FacadeTest {
+public class PersonTest {
     
-    public FacadeTest() {
+    public PersonTest() {
     }
     
     @Before
