1be386414e4fbce2ad4d8c7330d9895920241e17
Jörg <oerteljoerg@gmail.com>
2019-03-12 14:15:38 +0100

    Person Interface

---
+++ src/main/java/Interfaces/IPersonFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+/**
+ *
+ * @author oerte
+ */
+//public interface IPersonFacade {
+//
+//    // Return all Persons, with “all” details, as a JSON object 
+//    // api/person/complete
+//    public List<Person> getAllPerson();
+//
+//   // Return a Person (with the given id) with “all” details as a JSON object. Use the same DTO format as above
+//   // api/person/complete/id
+//    public Person getPersonById(int personId);
+//    
+//   //  Return all Persons, with only contact info, as a JSON object (As in JSON example-2)
+//  // api/person/contactinfo
+//    public List<Person> getAllPersonsByContactInfo();
+//    
+////  Create a new Person given a JSON object (Using this request it should be possible to create a person with address and phone)
+//// api/person
+//    public Person addNewPerson();
+
+
+    
+
