7e8245d583b0f3b587130c26aea7ca59aea6dd92
Porselicious <porseg69@gmail.com>
2019-03-14 14:38:56 +0100

    Exceptions are shit

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -6,6 +6,8 @@ import entity.Address;
 import entity.CityInfo;
 import entity.Person;
 import entity.Phone;
+import exceptions.ExceptionDTO;
+import exceptions.PersonNotFoundException;
 import facade.PersonFacade;
 import java.util.List;
 import javax.persistence.EntityManagerFactory;
@@ -16,10 +18,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -39,80 +44,109 @@ public class PersonResource {
     public PersonResource() {
     }
 
+    @GET
+    @Path("/test/ex")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String testExceptionDTO() {
+        try {
+            throw new NotFoundException("Person doesn't exist!");
+        } catch (NotFoundException e) {
+            ExceptionDTO exDTO = new ExceptionDTO(e, 406, true);
+            return gson.toJson(exDTO);
+        }
+
+    }
 
     @GET
     @Path("/all/complete")
     @Produces(MediaType.APPLICATION_JSON)
     public Response allPersonsAndInfo() {
         List<PersonDTO> allInfo = pf.getAllPersonsAndInfo();
+        if (allInfo == null) {
+            throw new NotFoundException("Could not find any persons.");
+        }
         return Response.ok().entity(gson.toJson(allInfo)).build();
+
     }
 
     @GET
     @Path("/complete/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getPersonById(@PathParam("id") int personId) {
-        return Response.ok().entity(gson.toJson(pf.getPersonByID(personId))).build();
+        PersonDTO p = null;
+        try {
+            p = pf.getPersonByID(personId);
+        } catch (PersonNotFoundException ex) {
+            throw new PersonNotFoundException("Person does not exist.");
+        }catch (WebApplicationException ex){
+            if(ex.)
+        }
+        return Response.ok().entity(gson.toJson(p)).build();
     }
 
     @GET
     @Path("/all/contactinfo")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getPersonsContactInfo() {
-        return Response.ok().entity(gson.toJson(pf.getAllPersonsContactInfo())).build();
+    public Response getPersonsContactInfo(){
+    List<PersonDTO> allInfo = pf.getAllPersonsContactInfo();
+        if (allInfo == null) throw new NotFoundException("Could not find any persons.");
+    
+        return Response.ok ()
+
+.entity(gson.toJson(allInfo)).build();
     }
 
     @GET
-    @Path("/phone/{phonenumber}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getPersonByPhoneNumber(@PathParam("phonenumber") int phone) {
+        @Path("/phone/{phonenumber}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response getPersonByPhoneNumber(@PathParam("phonenumber") int phone) {
         PersonDTO person = pf.getInfoFromPersonByPhoneNumber(phone);
         return Response.ok().entity(gson.toJson(person)).build();
     }
 
     @GET
-    @Path("/all/hobby/{hobby}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getPersonsByHobby(@PathParam("hobby") String hobby) {
+        @Path("/all/hobby/{hobby}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response getPersonsByHobby(@PathParam("hobby") String hobby) {
         List<PersonDTO> persons = pf.getAllPersonsByHobby(hobby);
         return Response.ok().entity(gson.toJson(persons)).build();
     }
 
     @GET
-    @Path("/all/city/{city}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getPersonsByCity(@PathParam("city") String city) {
+        @Path("/all/city/{city}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response getPersonsByCity(@PathParam("city") String city) {
         List<PersonDTO> persons = pf.getAllPersonsByCity(city);
         return Response.ok().entity(gson.toJson(persons)).build();
     }
 
     @GET
-    @Path("/contactinfo/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getSinglePersonContactInfo(@PathParam("id") int personId) {
+        @Path("/contactinfo/{id}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response getSinglePersonContactInfo(@PathParam("id") int personId) {
         return Response.ok().entity(gson.toJson(pf.getSinglePersonContactInfo(personId))).build();
     }
 
     @GET
-    @Path("/count/{hobby}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response countPersonsForHobby(@PathParam("hobby") String hobby) {
+        @Path("/count/{hobby}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response countPersonsForHobby(@PathParam("hobby") String hobby) {
         Long count = pf.getCountOfPeopleWithGivenHobby(hobby);
         return Response.ok().entity(gson.toJson(count)).build();
     }
 
     @DELETE
-    @Path("/delete/{id}")
-    public Response deletePersonById(@PathParam("id") int id) {
+        @Path("/delete/{id}")
+        public Response deletePersonById(@PathParam("id") int id) {
         pf.deletePersonById(id);
         return Response.ok().build();
     }
 
     @POST
-    @Path("/add")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response postPersonWithAddressAndPhone(String content, @QueryParam("phone") int phonenumber,
+        @Path("/add")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response postPersonWithAddressAndPhone(String content, @QueryParam("phone") int phonenumber,
             @QueryParam("pdescription") String pdescription,
             @QueryParam("street") String street,
             @QueryParam("sinfo") String sinfo,
@@ -123,7 +157,10 @@ public class PersonResource {
         Address address = new Address(street, sinfo);
         CityInfo city = new CityInfo(zipcode, cityname);
 
-        PersonDTO person = pf.postPersonWithAddressAndPhone(gson.fromJson(content, Person.class), phone, address, city);
+        PersonDTO 
+
+person = pf.postPersonWithAddressAndPhone(gson.fromJson(content, Person.class
+), phone, address, city);
         return Response.ok().entity(gson.toJson(person)).build();
     }
 
