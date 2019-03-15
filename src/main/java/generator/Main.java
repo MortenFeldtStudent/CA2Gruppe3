3076117632755a05fee7ff79232b12cb9f56d7b0
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2019-03-15 10:08:27 +0100

    Test Data Generator

--- src/main/java/generator/Main.java
+++ src/main/java/generator/Main.java
@@ -1,19 +1,43 @@
 package generator;
 
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Morten
+ */
 public class Main {
 
     public static void main(String[] args) {
 
-        int countSample = 10;
-
-        Generator generator = new Generator();
-
-        TestData testData;
-
-        testData = generator.generate(countSample);
-
-        for (String sqlStr : testData.getTestDataCollection()) {
-            System.out.println(sqlStr);
+        int countSample = 2000;
+        if (countSample % 1000 == 0) {
+            Generator generator = new Generator();
+            generator.initializeZipCodesAndCity();
+            final int countZipCodesInSQL = generator.amountOfZipCodes();
+            if (countSample >= countZipCodesInSQL) {
+                boolean noDublicates = true;
+                TestDataCollection testData = generator.generate(countSample, countZipCodesInSQL);
+                List<String> testDataGeneratedList = testData.getTestDataCollection();
+                for (int i = 0; i < testDataGeneratedList.size(); i++) {
+                    int amount = Collections.frequency(testDataGeneratedList, testDataGeneratedList.get(i));
+                    if (amount > 1) {
+                        noDublicates = false;
+                    }
+                }
+                if (noDublicates) {
+                    for (String sqlStr : testDataGeneratedList) {
+                        System.out.println(sqlStr);
+                    }
+                    System.out.println("Antal rækker af test data (uden ZipCodes): " + testDataGeneratedList.size());
+                } else {
+                    System.out.println("LISTEN INDEHOLDER DUBLETTER - PRØV IGEN!");
+                }
+            } else {
+                System.out.println("Du skal minimum sætte countSample til antal ZipCodes i SQL = " + countZipCodesInSQL);
+            }
+        } else {
+            System.out.println("Du skal vælge et sample size deleligt med 1000 (f.eks. 2000,3000 etc.)!!!");
         }
 
     }
---
+++ src/main/java/generator/ZipCodesTestData.java
@@ -0,0 +1,32 @@
+package generator;
+
+/**
+ * @author Morten
+ */
+public class ZipCodesTestData {
+
+    private String zipCode;
+    private String city;
+
+    public ZipCodesTestData(String zipCode, String city) {
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
